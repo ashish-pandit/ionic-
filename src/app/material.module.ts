ebf690409cd9111bae349951000432412031d2c2
Ashish Upadhyay <ashish@qilinlab.com>
2020-05-07 18:38:41 +0530

    second

--- src/app/material.module.ts
+++ src/app/material.module.ts
@@ -1,25 +1,22 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
-import {
-  MatButtonModule,
-  MatToolbarModule,
-  MatGridListModule,
-  MatTableModule,
-  MatPaginatorModule,
-  MatSortModule,
-  MatBadgeModule,
-  MatSelectModule,
-  MatSidenavModule,
-  MatIconModule,
-  MatListModule,
-  MatInputModule,
-  MatRippleModule,
-  MatDatepickerModule,
-  MatNativeDateModule,
-  MatExpansionModule,
-  MatSnackBarModule,
-  MatProgressBarModule,
-} from '@angular/material';
+import { MatBadgeModule } from '@angular/material/badge';
+import { MatButtonModule } from '@angular/material/button';
+import { MatRippleModule, MatNativeDateModule } from '@angular/material/core';
+import { MatDatepickerModule } from '@angular/material/datepicker';
+import { MatExpansionModule } from '@angular/material/expansion';
+import { MatGridListModule } from '@angular/material/grid-list';
+import { MatIconModule } from '@angular/material/icon';
+import { MatInputModule } from '@angular/material/input';
+import { MatListModule } from '@angular/material/list';
+import { MatPaginatorModule } from '@angular/material/paginator';
+import { MatProgressBarModule } from '@angular/material/progress-bar';
+import { MatSelectModule } from '@angular/material/select';
+import { MatSidenavModule } from '@angular/material/sidenav';
+import { MatSnackBarModule } from '@angular/material/snack-bar';
+import { MatSortModule } from '@angular/material/sort';
+import { MatTableModule } from '@angular/material/table';
+import { MatToolbarModule } from '@angular/material/toolbar';
 import { MatCheckboxModule } from '@angular/material/checkbox';
 @NgModule({
   declarations: [],
